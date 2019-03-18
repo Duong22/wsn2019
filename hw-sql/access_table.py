0ede41132139ef6445973a3c050a9c5056d072ac
Duong22 <thaiduonghp97@gmail.com>
2019-03-18 17:38:48 +0700

    command

--- hw-sql/access_table.py
+++ hw-sql/access_table.py
@@ -4,59 +4,20 @@ from datetime import datetime
 def show_data(name_tb):
 	db = MySQLdb.connect("localhost","boss","123456","mangcambien2019")
 	cursor = db.cursor()
-
 	sql = """SELECT * from %s""" % (name_tb)
-
 	cursor.execute(sql)
 	results = cursor.fetchall()
-
-	widths = []
-	columns = []
-	tavnit = '|'
-	separator = '+' 
-
-	for cd in cursor.description:
-	    widths.append(max(cd[2], len(cd[0])))
-	    columns.append(cd[0])
-
-	for w in widths:
-	    tavnit += " %-"+"%ss |" % (w,)
-	    separator += '-'*w + '--+'
-
-	print(separator)
-	print(tavnit % tuple(columns))
-	print(separator)
-	for row in results:
-	    print(tavnit % row)
-	print(separator)
+	print_result(results,cursor)
+	
 def show_max_min(name_tb):
 	db = MySQLdb.connect("localhost","boss","123456","mangcambien2019")
 	cursor = db.cursor()
-	name_tb = input("ENTER TABLE'S NAME: ")
 	name_col = input("ENTER COLUMN'S NAME: ")
-	sql = """SELECT MIN(%s) FROM %s; SELECT MAX(%s) FROM %s"""(name_col,name_tb,name_col,name_tb)
-	curosr.execute(sql)
+	sql = """SELECT MIN(%s) FROM %s; SELECT MAX(%s) FROM '%s'""" %(name_col,name_tb,name_col,name_tb)
+	cursor.execute(sql)
 	results = cursor.fetchall()
-	widths = []
-	columns = []
-	tavnit = '|'
-	separator = '+' 
-
-	for cd in cursor.description:
-	    widths.append(max(cd[2], len(cd[0])))
-	    columns.append(cd[0])
-
-	for w in widths:
-	    tavnit += " %-"+"%ss |" % (w,)
-	    separator += '-'*w + '--+'
-
-	print(separator)
-	print(tavnit % tuple(columns))
-	print(separator)
-	for row in results:
-	    print(tavnit % row)
-	print(separator)
-
+	print_result(results,cursor)	
+	
 def insert_value(name_tb):
 	db = MySQLdb.connect("localhost","boss","123456","mangcambien2019")
 	cursor = db.cursor()
@@ -77,3 +38,24 @@ def insert_value(name_tb):
 		print("SUCCESSFUL!")
 	except:
 		print("ERROR!")
+
+def print_result(results,cursor):
+	widths = []
+	columns = []
+	tavnit = '|'
+	separator = '+' 
+
+	for cd in cursor.description:
+	    widths.append(max(cd[2], len(cd[0])))
+	    columns.append(cd[0])
+
+	for w in widths:
+	    tavnit += " %-"+"%ss |" % (w,)
+	    separator += '-'*w + '--+'
+
+	print(separator)
+	print(tavnit % tuple(columns))
+	print(separator)
+	for row in results:
+	    print(tavnit % row)
+	print(separator)
--- hw-sql/menu.py
+++ hw-sql/menu.py
@@ -5,8 +5,8 @@ import table_manage
 
 choice_1 = True
 while choice_1:
-	print("********************WELCOME TO MYSQLDB********************")
-	print("\n1. CREATE NEW TABLE.\n2.SHOW TABLES.\n3.COPY TABLE.\n4.DELETE TABLE.\n5.SHOW DATA FROM TABLE.\n6.SHOW MAX, MIN.\n7.INSERT DATA.\n8.EXIT")
+	print("\n********************WELCOME TO MYSQLDB********************")
+	print("1. CREATE NEW TABLE.\n2.SHOW TABLES.\n3.COPY TABLE.\n4.DELETE TABLE.\n5.SHOW DATA FROM TABLE.\n6.SHOW MAX, MIN.\n7.INSERT DATA.\n8.EXIT")
 	choice_1 = input("SELECT YOUR CHOICE: ")
 
 	if choice_1 == "1":
@@ -18,13 +18,20 @@ while choice_1:
 	elif choice_1 == "4":
 		table_manage.drop_tb()
 	elif choice_1 == "5":
-		access_table.show_data()
+		name_tb = input("ENTER TABLE'S NAME: ")
+		access_table.show_data(name_tb)
 	elif choice_1 == "6":
 		name_tb = input("ENTER TABLE'S NAME: ")
 		access_table.show_max_min(name_tb)
 	elif choice_1 == "7":
 		name_tb = input("ENTER TABLE'S NAME: ")
 		access_table.insert_value(name_tb)
+	elif choice_1 == "8":
+		choice_1 = None
+		print ("GOOD BYE!")
+	else: print ("INVALID SYNTAX. PLEASE TRY AGAIN!")
+
+
 
 		
 
